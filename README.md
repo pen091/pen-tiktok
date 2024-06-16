f8fba31e8adb56a4d61f5e676c8d7ecd75c86f9a
pen091 <princeekenendioziri@gmail.com>
2024-06-16 10:34:28 +0000

    C programming language

---
+++ README.md
@@ -0,0 +1,10 @@
+		HOW TO COMPILE THE C VARIABLE CODE
+
+	Goto the directory :
+		/pen-tiktok/c_tutor
+
+	COMPILE :
+		gcc variable.c -o variable
+
+	RUN :
+		./variable
---
+++ tokens/c_tutor/variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{  /* this is an operator */
+	
+	/* int  ( keyword ) */
+	/* number ( identifier ) */
+	/* equal to ( operator ) */
+	/* value ( ascii ) separator */
+	int number = 200; /* variable declaration and initilzation */
+
+	printf("number is  : %d\n", number);
+
+	return 0;
+}
